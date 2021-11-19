87d13610daa1559152f181f76440f08d794f0eed
MrArmand <dellavmaster@gmail.com>
2021-11-19 16:36:25 +0000

    Added bomb methods

--- Bomb.java
+++ Bomb.java
@@ -5,5 +5,15 @@ public class Bomb extends Item {
 		this.setLocation(x, y);
 	}
 	
+	public void startTimer() {
+		//after few ticks bomb explodes
+		explode();
+	}
+	
+	private void explode() {
+		//3x3 Tiles.getDamage();
+		Bomb disappears;
+	}
+	
 
 }
