9eb9f538ac562184acc4b016f679a32b112d1b78
Calcoomba <1904825@swansea.ac.uk>
2021-11-24 18:39:40 +0000

    Updated Bomb and ItemManager Test class

--- Main.java
+++ Main.java
@@ -68,13 +68,26 @@ public class Main extends Application {
 	private Image grass = new Image("TestTextures/grass.png");
 	private Image path = new Image("TestTextures/path.png");
 	private Image tunnel = new Image("TestTextures/tunnel.png");
+	private Image bomb;
+	private Image gas;
+	private Image poison;
+	private Image noEntrySign;
+	private Image sterillisation;
+	private Image maleSexChanger;
+	private Image femaleSexChanger;
+	private Image deathRat;
 	
 	/**
 	 * Setup the new application.
 	 * @param primaryStage The stage that is to be used for the application.
 	 */
 	public void start(Stage primaryStage) {		
-		
+		gas = new Image("/items/gas.png");
+		poison = new Image("/items/poison.png");
+		noEntrySign = new Image("/items/noentrysign.png");
+		maleSexChanger = new Image("/items/malesexchanger.png");
+		femaleSexChanger = new Image("/items/malesexchanger.png");
+		deathRat = new Image("/items/deathrat.png");
 
 		// Build the GUI 
 		Pane root = buildGUI();
@@ -95,14 +108,7 @@ public class Main extends Application {
 		// Display the scene on the stage
 		//testLevel.addRenderObject(new BabyRat(new Position(2,2), false, testLevel));
 		//testLevel.addRenderObject(new BabyRat(new Position(5,5), false, testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(1,1), testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(2,2), testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(3,2), testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(4,2), testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(5,2), testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(6,2), testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(7,2), testLevel));
-		testLevel.addRenderObject(new Bomb(new Position(8,2), testLevel));
+		
 		drawGame();
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -161,6 +167,38 @@ public class Main extends Application {
 		//We then redraw the whole canvas.
 		drawGame();
 	}
+	public void canvasDragDroppedOccured(DragEvent event,Image img) {
+    	double x = event.getX();
+        double y = event.getY();
+
+        // Print a string showing the location.
+        String s = String.format("You dropped at (%f, %f) relative to the canvas.", x, y);
+    	System.out.println(s);
+
+    	// Draw an icon at the dropped location.
+    	GraphicsContext gc = canvas.getGraphicsContext2D();
+    	// Draw the the image so the top-left corner is where we dropped.
+    	//gc.drawImage(iconImage, x, y);
+    	// Draw the the image so the center is where we dropped.    
+    	
+    	
+    	 
+    	
+    	gc.drawImage(img, x - img.getWidth() / 2.0, y - img.getHeight() / 2.0);
+	}
+	public void bombDropOccured(DragEvent event) {
+    	double x = event.getX();
+        double y = event.getY();
+
+        // Print a string showing the location.
+        String s = String.format("You dropped at (%f, %f) relative to the canvas.", x, y);
+    	System.out.println(s);
+    	
+    	testLevel.addRenderObject(new Bomb(new Position(x,y), testLevel));
+	}
+	
+	
+	
 	
 	/**
 	 * Create the GUI.
@@ -175,6 +213,129 @@ public class Main extends Application {
 		canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 		root.setCenter(canvas);
         
+		HBox toolbar = new HBox();
+		toolbar.setSpacing(10);
+		toolbar.setPadding(new Insets(10, 10, 10, 10)); 
+		root.setBottom(toolbar);
+		
+		ImageView draggableImage = new ImageView();
+		draggableImage.setImage(bomb);
+        toolbar.getChildren().add(draggableImage);
+        draggableImage.setOnDragDetected(new EventHandler<MouseEvent>() {
+		    public void handle(MouseEvent event) {
+		        // Mark the drag as started.
+		    	// We do not use the transfer mode (this can be used to indicate different forms
+		    	// of drags operations, for example, moving files or copying files).
+		    	Dragboard db = draggableImage.startDragAndDrop(TransferMode.ANY);
+
+		    	// We have to put some content in the clipboard of the drag event.
+		    	// We do not use this, but we could use it to store extra data if we wished.
+                ClipboardContent content = new ClipboardContent();
+                content.putString("Hello");
+                db.setContent(content);
+                
+		    	// Consume the event. This means we mark it as dealt with. 
+		        event.consume();
+		    }
+		});
+		
+        ImageView draggableImage2 = new ImageView();
+		draggableImage2.setImage(gas);
+        toolbar.getChildren().add(draggableImage2);
+        draggableImage2.setOnDragDetected(new EventHandler<MouseEvent>() {
+		    public void handle(MouseEvent event) {
+		        // Mark the drag as started.
+		    	// We do not use the transfer mode (this can be used to indicate different forms
+		    	// of drags operations, for example, moving files or copying files).
+		    	Dragboard db = draggableImage2.startDragAndDrop(TransferMode.ANY);
+
+		    	// We have to put some content in the clipboard of the drag event.
+		    	// We do not use this, but we could use it to store extra data if we wished.
+                ClipboardContent content = new ClipboardContent();
+                content.putString("Hello");
+                db.setContent(content);
+                
+		    	// Consume the event. This means we mark it as dealt with. 
+		        event.consume();
+		    }
+		});
+        
+        ImageView draggableImage3 = new ImageView();
+		draggableImage3.setImage(poison);
+        toolbar.getChildren().add(draggableImage3);
+        draggableImage3.setOnDragDetected(new EventHandler<MouseEvent>() {
+		    public void handle(MouseEvent event) {
+		        // Mark the drag as started.
+		    	// We do not use the transfer mode (this can be used to indicate different forms
+		    	// of drags operations, for example, moving files or copying files).
+		    	Dragboard db = draggableImage3.startDragAndDrop(TransferMode.ANY);
+
+		    	// We have to put some content in the clipboard of the drag event.
+		    	// We do not use this, but we could use it to store extra data if we wished.
+                ClipboardContent content = new ClipboardContent();
+                content.putString("Hello");
+                db.setContent(content);
+                
+		    	// Consume the event. This means we mark it as dealt with. 
+		        event.consume();
+		    }
+		});
+		
+        // This code allows the canvas to receive a dragged object within its bounds.
+        // You probably don't need to change this (unless you wish to do more advanced things).
+        canvas.setOnDragOver(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+		        // Mark the drag as acceptable if the source was the draggable image.
+            	// (for example, we don't want to allow the user to drag things or files into our application)
+            	if (event.getGestureSource() == draggableImage) {
+    		    	// Mark the drag event as acceptable by the canvas.
+            		event.acceptTransferModes(TransferMode.ANY);
+    		    	// Consume the event. This means we mark it as dealt with.
+            		event.consume();
+            	} else if (event.getGestureSource() == draggableImage2) {
+    		    	// Mark the drag event as acceptable by the canvas.
+            		event.acceptTransferModes(TransferMode.ANY);
+    		    	// Consume the event. This means we mark it as dealt with.
+            		event.consume();
+            	} else if (event.getGestureSource() == draggableImage3) {
+    		    	// Mark the drag event as acceptable by the canvas.
+            		event.acceptTransferModes(TransferMode.ANY);
+    		    	// Consume the event. This means we mark it as dealt with.
+            		event.consume();
+            		
+            	}
+            }
+        });
+        
+        // This code allows the canvas to react to a dragged object when it is finally dropped.
+        // You probably don't need to change this (unless you wish to do more advanced things).
+        canvas.setOnDragDropped(new EventHandler<DragEvent>() {
+            public void handle(DragEvent event) {
+            	// We call this method which is where the bulk of the behaviour takes place.
+            	if (event.getGestureSource() == draggableImage) {
+    		    	// Mark the drag event as acceptable by the canvas.
+            		bombDropOccured(event);
+    		    	// Consume the event. This means we mark it as dealt with.
+            		event.consume();
+            	} else if (event.getGestureSource() == draggableImage2) {
+    		    	// Mark the drag event as acceptable by the canvas.
+            		canvasDragDroppedOccured(event,gas);
+    		    	// Consume the event. This means we mark it as dealt with.
+            		event.consume();
+            	} else if (event.getGestureSource() == draggableImage3) {
+    		    	// Mark the drag event as acceptable by the canvas.
+            		canvasDragDroppedOccured(event,poison);
+    		    	// Consume the event. This means we mark it as dealt with.
+            		event.consume();
+            	}
+    
+            	
+            	// Consume the event. This means we mark it as dealt with.
+            	event.consume();
+             }
+        });
+		
+		
 		// Finally, return the border pane we built up.
         return root;
 	}
