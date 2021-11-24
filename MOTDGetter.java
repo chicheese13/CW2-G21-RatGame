1b338904ab8d7af7bc235184a9e92ccf345022df
Lorenzo Labarta <76446788+17012002@users.noreply.github.com>
2021-11-24 16:53:24 +0000

    Update MOTDGetter

--- MOTDGetter.java
+++ MOTDGetter.java
@@ -1,3 +1,28 @@
-public class MOTDGetter {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
+public class MOTDGetter {
+    public static String returnText(String text) {
+        File messages = new File("messages.txt");
+
+        ArrayList<String> texts = new ArrayList<String>();
+
+        try {
+            Scanner in = new Scanner(messages);
+
+            while (in.hasNextLine()) {
+                texts.add(in.nextLine());
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Random randomNumber = new Random();
+
+        return texts.get(randomNumber.nextInt(texts.size()));
+    }
 }
