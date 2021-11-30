2aca68148b3df56d8db2284bcafd58f874f45d7c
MrArmand <dellavmaster@gmail.com>
2021-11-30 22:34:57 +0000

    Updated the Main

--- MainItems.java
+++ MainItems.java
@@ -51,15 +51,15 @@ public class MainItems extends Application {
 	// The dimensions of the canvas
 	private static final int CANVAS_WIDTH = WINDOW_WIDTH;
 	private static final int CANVAS_HEIGHT = WINDOW_HEIGHT;
+	
+	TestLevel testLevel = new TestLevel();
+	
+	// Timeline which will cause tick method to be called periodically.
+	private Timeline tickTimeline;
 
 	// The canvas in the GUI. This needs to be a global variable
 	// (in this setup) as we need to access it in different methods.
 	private Canvas canvas;
-
-	// Timeline which will cause tick method to be called periodically.
-	private Timeline tickTimeline;
-
-	TestLevel testLevel = new TestLevel();
 	// testLevel.addRenderObject(new BabyRat(new Position(2,2), false, testLevel));
 
 	// BabyRat testRat = new BabyRat(new Position(2,2), false, testLevel);
@@ -94,90 +94,176 @@ public class MainItems extends Application {
 		sterilisation = new Image("/items/sterilisation.png");
 		
 
-		// Build the GUI
-		Pane root = buildGUI();
+		// Build the GUI 
+				Pane root = buildGUI();
+				
+				// Create a scene from the GUI
+				Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+						
+				// Register a tick method to be called periodically.
+				// Make a new timeline with one keyframe that triggers the tick method every half a second.
+				tickTimeline = new Timeline(new KeyFrame(Duration.millis(15), event -> tick()));
+				
+				 // Loop the timeline forever
+				tickTimeline.setCycleCount(Animation.INDEFINITE);
+				
+				// We start the timeline upon a button press.
+				tickTimeline.play();
+				
+				// Display the scene on the stage
+				testLevel.addRenderObject(new BabyRat(new Position(new BigDecimal("2"), new BigDecimal("1")), false, testLevel, 'N'));
+				testLevel.addRenderObject(new BabyRat(new Position(new BigDecimal("2"), new BigDecimal("1")), true, testLevel, 'N'));
+				
 
-		// Create a scene from the GUI
-		Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+				testLevel.addRenderObject(new AdultRat(new Position(new BigDecimal("4"), new BigDecimal("1")), true, false, 10, 'N', testLevel));
+				testLevel.addRenderObject(new AdultRat(new Position(new BigDecimal("4"), new BigDecimal("1")), false, false, 10, 'N', testLevel));
 
-		// Register a tick method to be called periodically.
-		// Make a new timeline with one keyframe that triggers the tick method every
-		// half a second.
-		tickTimeline = new Timeline(new KeyFrame(Duration.millis(15), event -> tick()));
-
-		// Loop the timeline forever
-		tickTimeline.setCycleCount(Animation.INDEFINITE);
-
-		// We start the timeline upon a button press.
-		tickTimeline.play();
-
-		// Display the scene on the stage
-		// testLevel.addRenderObject(new BabyRat(new Position(2,2), false, testLevel));
-		// testLevel.addRenderObject(new BabyRat(new Position(5,5), false, testLevel));
-		drawGame();
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+				testLevel.addRenderObject(new AdultRat(new Position(new BigDecimal("4"), new BigDecimal("1")), false, false, 10, 'N', testLevel));
+				
+				testLevel.addRenderObject(new DeathRat(new Position(new BigDecimal("7"), new BigDecimal("1")), testLevel));
+				//testLevel.addRenderObject(new DeathRat(new Position(new BigDecimal("1"), new BigDecimal("4")), testLevel));
+				
+				//testLevel.addRenderObject(new BabyRat(new Position(2,1), true, testLevel, 'N'));
+				
+				//AdultRat testRat = new AdultRat(new Position(1,1), true, false, 10, 0, 'E', testLevel);
+				//testRat.becomePregnant();
+				
+				//testLevel.addRenderObject(new AdultRat(new Position(new BigDecimal("2"), new BigDecimal("1"))), true, false, 10, 0, 'N', testLevel));
+				//testLevel.addRenderObject(new AdultRat(new Position(3,1), false, false, 10, 0, 'N', testLevel));
+				//testLevel.addRenderObject(testRat);
+				//testLevel.addRenderObject(new BabyRat(new Position(5,5), false, testLevel));
+				
+				//System.out.println(testLevel.tileAvailable(0, 0, 'N'));
+				
+				//Position position, boolean gender, boolean sterile, int ratHealth, double tickIn, char direction, TestLevel currentLevel
+				
+				//testLevel.addRenderObject(testRat);
+				//testLevel.addRenderObject(testRatTwo);
+				
+				//System.out.println("test----);");
+				//System.out.println(testRat.cleanDecimal(new BigDecimal("0.69"), new BigDecimal("0.04")));
+				
+				drawGame();
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			}
 
 	/**
 	 * Draw the game on the canvas. This creates a frame, with all the tiles, items
 	 * and rats.
 	 */
+	/**
+	 * Draw the game on the canvas.
+	 * This creates a frame, with all the tiles, items and rats.
+	 */
 	public void drawGame() {
-
+		
 		// Get the Graphic Context of the canvas. This is what we draw on.
 		GraphicsContext gc = canvas.getGraphicsContext2D();
-
+		
 		// Clear canvas
 		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
+		
 		// Set the background to gray.
 		gc.setFill(Color.GRAY);
 		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-		// this goes through the tile array and generates the tile images on the canvas
-		// based on the tiles in the array.
+	
+		//this goes through the tile array and generates the tile images on the canvas based on the tiles in the array.
 		for (int y = 0; y < testLevel.getTiles().length; y++) {
 			for (int x = 0; x < testLevel.getTiles()[0].length; x++) {
-				// checks what tile type it is and outputs the image for that tile, in the x and
-				// y of that tile.
+				//checks what tile type it is and outputs the image for that tile, in the x and y of that tile.
 				if (testLevel.getTiles()[y][x] == "G") {
 					gc.drawImage(grass, x * GRID_CELL_WIDTH, y * GRID_CELL_HEIGHT);
-				} else if (testLevel.getTiles()[y][x] == "P") {
+				} else if (testLevel.getTiles()[y][x] ==  "P") {
 					gc.drawImage(path, x * GRID_CELL_WIDTH, y * GRID_CELL_HEIGHT);
 				} else {
 					gc.drawImage(tunnel, x * GRID_CELL_WIDTH, y * GRID_CELL_HEIGHT);
 				}
 			}
 		}
-
+		
 		for (int i = 0; i < testLevel.getRenderObjects().size(); i++) {
-			// System.out.println(testLevel.getRenderObjects().get(i).getSprite());
-			// gc.drawImage(testLevel.getRenderObjects().get(i).getSprite(),
-			// testLevel.getRenderObjects().get(i).getPosition()[0] * GRID_CELL_WIDTH,
-			// testLevel.getRenderObjects().get(i).getPosition()[1] * GRID_CELL_HEIGHT);
-
-			BigDecimal x = ((testLevel.getRenderObjects().get(i).getObjectPosition()[0].multiply(BigDecimal.valueOf(GRID_CELL_WIDTH))));
-
-			BigDecimal y = ((testLevel.getRenderObjects().get(i).getObjectPosition()[1].multiply(BigDecimal.valueOf(GRID_CELL_HEIGHT))));
-
-			gc.drawImage(testLevel.getRenderObjects().get(i).getSprite(), x.doubleValue(), y.doubleValue());
+			//System.out.println(testLevel.getRenderObjects().get(i).getSprite());
+			//gc.drawImage(testLevel.getRenderObjects().get(i).getSprite(), testLevel.getRenderObjects().get(i).getPosition()[0] * GRID_CELL_WIDTH, testLevel.getRenderObjects().get(i).getPosition()[1] * GRID_CELL_HEIGHT);
+			gc.drawImage(testLevel.getRenderObjects().get(i).getSprite(), testLevel.getRenderObjects().get(i).getObjectPosition()[0].doubleValue() * GRID_CELL_WIDTH, testLevel.getRenderObjects().get(i).getObjectPosition()[1].doubleValue() * GRID_CELL_HEIGHT);
 		}
-
+		
 	}
-
+	
 	/**
-	 * This method is called periodically by the tick timeline and would for,
-	 * example move, perform logic in the game, this might cause the bad guys to
-	 * move (by e.g., looping over them all and calling their own tick method).
+	 * This method is called periodically by the tick timeline
+	 * and would for, example move, perform logic in the game,
+	 * this might cause the bad guys to move (by e.g., looping
+	 * over them all and calling their own tick method). 
 	 */
+	private int counter = 0;
 	public void tick() {
-		// Here we will do the tick method for items and rats.
-		// Likely to have an array of objects which we call the tick method on.
+		
+		System.out.println(testLevel.getRenderObjects().size());
+		//Here we will do the tick method for items and rats.
+		//Likely to have an array of objects which we call the tick method on.
+		
+		//maybe a recursive algorithm which removes collide objects from the list that have already had a comparison
+		counter++;
+		System.out.println(testLevel.getScore());
+		if (counter == 200) {
+			//testRatTwo.ratDeath();
+		}
+		
+		//testLevel.getRenderObjects().size() = testLevel.getRenderObjects().size() - 700;
+		
 		for (int i = 0; i < testLevel.getRenderObjects().size(); i++) {
 			testLevel.getRenderObjects().get(i).tick();
 		}
-		// We then redraw the whole canvas.
+		
+		for (int i = 0; i < testLevel.getRenderObjects().size(); i++) {
+			
+			double xMinus = testLevel.getRenderObjects().get(i).getObjectPosition()[0].doubleValue() - 0.5;
+			double yMinus = testLevel.getRenderObjects().get(i).getObjectPosition()[1].doubleValue() - 0.5;
+			double xPlus = testLevel.getRenderObjects().get(i).getObjectPosition()[0].doubleValue() + 0.5;
+			double yPlus = testLevel.getRenderObjects().get(i).getObjectPosition()[1].doubleValue() + 0.5;
+			
+			
+			
+			//checking if the the render objects are both rats and are not the same rat.
+			for (int i2 = 0; i2 < testLevel.getRenderObjects().size(); i2++) {
+				//testLevel.getRenderObjects().get(i).getObjectPosition()[0]
+				
+				boolean xCollide = false;
+				boolean yCollide = false;
+				
+				if (testLevel.getRenderObjects().get(i) != testLevel.getRenderObjects().get(i2)
+					&& testLevel.getRenderObjects().get(i2) instanceof Rat
+					&& testLevel.getRenderObjects().get(i) instanceof Rat) {
+					
+					double compareX = testLevel.getRenderObjects().get(i2).getObjectPosition()[0].doubleValue();
+					double compareY = testLevel.getRenderObjects().get(i2).getObjectPosition()[1].doubleValue();
+					
+					if (compareX > xMinus && compareX < xPlus) {
+						xCollide = true;
+						//System.out.println("X COLLIDE");
+					}
+					
+					if (compareY > yMinus && compareY < yPlus) {
+						yCollide = true;
+						//System.out.println("Y COLLIDE");
+					}
+					
+					if (xCollide == true && yCollide == true) {
+						//System.out.println(collisionCounter);
+						
+						((Rat) testLevel.getRenderObjects().get(i)).collision((Rat) testLevel.getRenderObjects().get(i2));
+						//((AdultRat) testLevel.getRenderObjects().get(i2)).collision((AdultRat) testLevel.getRenderObjects().get(i));
+					} else {
+						//System.out.println("");
+					}
+					
+				}
+				
+			}
+		}
+		
+		//We then redraw the whole canvas.
 		drawGame();
 	}
 
@@ -237,7 +323,7 @@ public class MainItems extends Application {
 		String s = String.format("You dropped at (%f, %f) relative to the canvas.", x, y);
 		System.out.println(s);
 
-		testLevel.addRenderObject(new DeathRatSpawner(new Position(BigDecimal.valueOf(x),BigDecimal.valueOf(y)), testLevel));
+		testLevel.addRenderObject(new DeathRat(new Position(BigDecimal.valueOf(x),BigDecimal.valueOf(y)), testLevel));
 		drawGame();
 	}
 	
