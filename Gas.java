974a0d3b7e662935ace7e238b9136f26eb73488e
thedylemma <92330171+thedylemma@users.noreply.github.com>
2021-12-02 21:03:06 +0000

    Adjusted bomb sound

--- Gas.java
+++ Gas.java
@@ -12,55 +12,20 @@ public class Gas extends CollideItem {
 		this.currentLevel = currentLevel;
 	}
 
-	private Image loadImage(int pictureNumber) {
 
-		Image gas = new Image("/gas_images/Sprinkler" + String.valueOf(pictureNumber) + ".png");
 
-		return gas;
-	}
-
-	public void startGas() {
-		// starts gas
-	}
-
-	public void tick() {
-
-		endCounter = endCounter + 12.5;
-
-		if (tickCounter >= 60) {
-
-			tickCounter = 0;
-
-			if (pictureNumber == 5) {
-				pictureNumber = 1;
-			} else {
-				pictureNumber++;
-			}
-			this.renderSprite = loadImage(pictureNumber);
-		} else if (endCounter >= 4000) {
-			this.currentLevel.despawnItem(this);
-
-		} else {
-			tickCounter = tickCounter + 15;
-		}
-
-	}
-
-	
-	public void movement() {
-		
-	}
-	
-	public void pathFinding() {
-		//path finding algorithm for gas
-		
-		//pick every available tile
-		//move in that direction
-		
-	}
-	
-
+	@Override
 	public void collision(Object collidedObject) {
-
+		// TODO Auto-generated method stub
+		
 	}
+
+
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
