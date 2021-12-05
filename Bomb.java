644b483a8c1c76853d583c3d11d19dd68c53552c
chicheese13 <2014480@swansea.ac.uk>
2021-12-05 16:26:50 +0000

    Revert "Merge branch 'main' of https://github.com/chicheese13/CW2-G21-RatGame"
    This reverts commit 5e050297cd44d5872569c67fdd39f48083b98ace, reversing
    changes made to 7394f007fc4fc1f684e63f14ef29c634dcb012e1.

--- Bomb.java
+++ Bomb.java
@@ -10,6 +10,7 @@ public class Bomb extends Item {
 
 	private double tickCounter = 0;
 	private double delayCount = 0;
+	private int frameTime = 0;
 	private int pictureNumber = 1;
 	private int delayedPictureNumber = 4;
 	private boolean timerStarted = false;
@@ -40,60 +41,60 @@ public class Bomb extends Item {
 	}
 
 	public void tick() {
+		
 
 		if (timerStarted == false) {
-
-			if (delayCount == 0) {
-
+			
+			if(delayCount == 0) {
+				
 				this.currentLevel.spawnSound("BombBackground");
 			}
 
 			if (delayCount % 1000 == 0) {
 				this.currentLevel.spawnSound("Tick");
 				this.renderSprite = loadDelayImage(delayedPictureNumber);
-
 				if (delayedPictureNumber > 1) {
 					delayedPictureNumber--;
+
 				}
 			}
 
 			delayCount = delayCount + 12.5;
 
-			if (delayCount >= 4000) {
-				;
+			if (delayCount >= 4000 && delayCount < 4020) {
+				//background.stop();
 				startTimer();
 				timerStarted = true;
 			}
 		} else {
 
 			if ((tickCounter == 3)) {
-
+				
 				tickCounter = 0;
-
-				this.renderSprite = loadImage(pictureNumber);
-
-				if (pictureNumber < NUMBER_OF_BOMB_FRAMES) {
-					pictureNumber++;
-				}
-
+				
 				if (pictureNumber == EXPLOSION_FRAME) {
 					explode();
 					this.currentLevel.spawnSound("Explosion");
+				}
+
+				this.renderSprite = loadImage(pictureNumber);
+				if (pictureNumber < NUMBER_OF_BOMB_FRAMES) {
+					pictureNumber++;
 				}
 			}
 
 			tickCounter++;
 
-			if (pictureNumber == NUMBER_OF_BOMB_FRAMES) {
-
+			if (pictureNumber == NUMBER_OF_BOMB_FRAMES ) {
+				
 				this.currentLevel.despawnItem(this);
 			}
-
 		}
 	}
 
 	// Method to deal damage horizontally and vertically
-	private void explode() {		
+	private void explode() {
+		System.out.println("BOOM");
 
 		// spawns new items for every every available tile on the x and y coords.
 		this.currentLevel.spawnTempTile(new Explosion(
