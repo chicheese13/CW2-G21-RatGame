43a32dbcffcf75f91678a02f6f2df3516cc15378
muht123 <joshua@postbag.net>
2021-12-05 17:44:28 +0000

    commenting

--- Leaderboard.java
+++ Leaderboard.java
@@ -9,13 +9,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
-import java.util.function.LongFunction;
 
 /**
- * Leaderboard is a class which manages the Elements that
+ * Leaderboard is a class which manages the high scores of players from past
+ * games
  */
 public class Leaderboard {
     /**
@@ -39,14 +38,13 @@ public class Leaderboard {
     }
 
     /**
-     * tries to read the text file with the scores, splitted by " ". And builds the
+     * tries to read the text file with the scores, splitted by ", ". And builds the
      * LeaderboardElement with the data.
      * 
      * @throws Exception e
      */
     private void readBoard() {
-        try {
-            Scanner in = new Scanner(scoreFile);
+        try (Scanner in = new Scanner(scoreFile);) {
             while (in.hasNextLine()) {
                 String text = in.nextLine();
                 String[] details = text.split(", ");
@@ -54,18 +52,14 @@ public class Leaderboard {
                         Integer.parseInt(details[2]));
                 allScores.add(score);
             }
-            in.close();
         } catch (Exception e) {
-            // TODO: Catch exception. Lorenzo here-> Hi! Check if this how you wanted the
-            // exception to work. I've alse created javaDoc for the LeaderboardElement and
-            // checked the MOTDGetter2 comments.
+            System.out.println("unable to read high scores");
         }
     }
 
     /**
-     * iterates through the ArrayList of scores and if the level is equal it adds
-     * the score to the priority queue.
-     * iterates through the PriorityQueue and removes them
+     * Populates the levelScores array with the highscores from allscores array and
+     * removes these from the allScores array
      */
     private void populateLevelScores() {
         for (LeaderboardElement score : allScores) {
@@ -79,7 +73,8 @@ public class Leaderboard {
     }
 
     /**
-     * updates the scores into the arraylist.
+     * updates the top 10 high scores back into the allsScores array, including
+     * newly added score
      */
     private void updateAllScores() {
         while (!levelScores.isEmpty()) {
@@ -88,7 +83,7 @@ public class Leaderboard {
     }
 
     /**
-     * creates the board with the scores. And shows it on screen.
+     * Writes the allScores array to the textfile.
      */
     private void writeBoard() {
         clearFile();
@@ -115,6 +110,12 @@ public class Leaderboard {
         }
     }
 
+    /**
+     * addds the new score to the levelScore array and trims the levelScore down to
+     * the top 10 scores if necessary
+     * 
+     * @param newScore the LeaderboardElement which contains the new score to add
+     */
     private void addScore(LeaderboardElement newScore) {
         levelScores.add(newScore);
         LeaderboardElement[] temp = new LeaderboardElement[10];
@@ -129,22 +130,17 @@ public class Leaderboard {
         }
     }
 
-    /*
-     * prints the scores of the PriorityQueue.
-     * adds the scores to the PriorityQueue from temp.
-     *
-     * public void displayQueue() {
-     * LeaderboardElement[] temp = new LeaderboardElement[10];
-     * for (int i = 0; i < 10 && !levelScores.isEmpty(); i++) {
-     * temp[i] = levelScores.poll();
-     * System.out.println(temp[i].toString());
-     * }
-     * for (LeaderboardElement score : temp) {
-     * if (score != null) {
-     * levelScores.add(score);
-     * }
-     * }
-     * }
+    /**
+     * The run method takes a user name and score, and runs through a full sequence
+     * that takes the board for the curentlevel from the scores file, adds the new
+     * score, updates the new levelScores array up to allScores and then rewrites
+     * the scores file to add the changes tht have been made. And finally it returns
+     * the levelScore array to be used to display the score to screen
+     * 
+     * @param name  The name of the user who set the score
+     * @param score The score achieved
+     * @return PriorityQueue<LeaderboardElement> the final leaderboard showingt the
+     *         top10 scores for the completed level
      */
     // runs it
     public PriorityQueue<LeaderboardElement> run(String name, int score) {
