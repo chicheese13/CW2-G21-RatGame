6a36bd49cdc91804fc32355a38049f3ba5b9d363
muht123 <joshua@postbag.net>
2021-12-06 09:04:36 +0000

    Revert "Update SterileRadius.java"
    This reverts commit 72cde680a663df93ed4d5d5637de7842d64dafa8.

--- SterileRadius.java
+++ SterileRadius.java
@@ -1,52 +1,49 @@
+
 /**
- * @version 1.0*
+ * @version 1.0
  * @author Dylan Lewis
  * 
+ *         SterileRadius class, which makes rat in certain radius sterile
  *
- *         SterileRadius class,which makes rat in certain radius sterile
- **/
-public class SterileRadius
-        extends RenderObject {
+ */
+public class SterileRadius extends RenderObject {
 
-    private Sterilisation sterileParent;
-    private final int STERILE_DURATION = 200;
+	private Sterilisation sterileParent;
+	private final int STERILE_DURATION = 200;
 
-    /*
-     * Instantiates a new SterileRadius**
-     * 
-     * @param position
-     * 
-     * @param currentLevel
-     * 
-     * @param parent
-     * 
-     * @param image
-     */
-    public SterileRadius(Position position, Level currentLevel, Sterilisation parent, String image) {
-        this.objectPosition = position;
-        this.currentLevel = currentLevel;
-        this.sterileParent = parent;
+	/**
+	 * Instantiates a new SterileRadius
+	 * 
+	 * @param position
+	 * @param currentLevel
+	 * @param parent
+	 * @param image
+	 */
+	public SterileRadius(Position position, Level currentLevel, Sterilisation parent, String image) {
+		this.objectPosition = position;
+		this.currentLevel = currentLevel;
+		this.sterileParent = parent;
 
-        if (!"".equals(image)) {
-            this.renderSprite = image;
-        } else {
-            this.renderSprite = "tr";
-        }
+		if (image != "") {
+			this.renderSprite = image;
+		} else {
+			this.renderSprite = "tr";
+		}
 
-    }
+	}
 
-    public void collision(Object collidedObject) {
-        //
-    }
+	public void collision(Object collidedObject) {
 
-    /**
-     * Method for sterilisation of rats, which step into radius
-     */
-    @Override
-    public void tick() {
-        System.out.println(this.sterileParent.getTick());
-        if (this.sterileParent.getTick() >= STERILE_DURATION - 1) {
-            this.currentLevel.despawnTempTile(this);
-        }
-    }
+	}
+
+	/**
+	 * Method for sterilisation of rats, which step into radius
+	 */
+	@Override
+	public void tick() {
+		System.out.println(this.sterileParent.getTick());
+		if (this.sterileParent.getTick() >= STERILE_DURATION - 1) {
+			this.currentLevel.despawnTempTile(this);
+		}
+	}
 }
